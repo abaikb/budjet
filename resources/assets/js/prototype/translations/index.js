2a9738ec7f7845f517e30b4ce71a758f79c647a2
abaikb <abaikb@icloud.com>
2024-08-06 21:23:14 +0600

    fix bag

--- resources/assets/js/prototype/translations/index.js
+++ resources/assets/js/prototype/translations/index.js
@@ -1,9 +1,10 @@
 import * as en from './en.json';
 import * as nl from './nl.json';
-import * as nl from './ru.json';
+import * as ru from './ru.json';
 
 
 export default {
     en,
     nl,
+    ru,
 };
